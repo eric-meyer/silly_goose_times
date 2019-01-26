16999f955883d6cfcb5acf2210176baa3a715475
eric-meyer <45677422+eric-meyer@users.noreply.github.com>
2019-01-26 14:33:18 -0600

    Add files via upload

---
+++ Chapter02/2_1.cpp
@@ -0,0 +1,19 @@
+/*Write a program that stores the integers 50 and 100 in variables
+and stores the sum of these two in a variable named total*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    //initialize x
+    int x = 50;
+    //initialize y
+    int y = 100;
+    //define total
+    int total = x + y;
+
+    //output total
+    cout<<total<<endl;
+    return 0;
+}
